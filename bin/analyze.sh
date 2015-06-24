1ab1dc6d703d5ebfb5f6d3db6ba368190685aa53
Denjell <denjell@das-gaengeviertel.info>
2015-06-24 15:20:48 +0200

    now using BSD-style sed - Closes #3

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -8,7 +8,7 @@ sensitivity=$2
 
 mplayer -really-quiet -vo null -ao null -frames 0 -identify "${movie}" | grep 'ID_VIDEO_FPS' | cut -d'=' -f2 > ${movie}.edl
 
-ffprobe -loglevel error -show_frames -of compact=p=0 -f lavfi "movie=${movie},select=gt(scene\,${sensitivity})" | sed -r 's/.*pkt_pts_time=([0-9.]{8,})\|.*/\1/' >> ${movie}.tmp
+ffprobe -loglevel error -show_frames -of compact=p=0 -f lavfi "movie=${movie},select=gt(scene\,${sensitivity})" | sed -E 's/.*pkt_pts_time=([0-9.]{8,})\|.*/\1/' >> ${movie}.tmp
 
 cat ${movie}.tmp >> ${movie}.edl
 rm ${movie}.tmp
